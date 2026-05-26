309c619dc603f6e738750a9d9fe0fedb41facc70
Joel Nolten <j.j.v.nolten@student.tue.nl>
2026-05-26 14:49:35 +0200

    8.1 Add a 4th pose X(4)

--- src/add_pose.py
+++ src/add_pose.py
@@ -9,8 +9,17 @@ ODOMETRY_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.2, 0.2, 0.1]))  # 
 MEASUREMENT_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.05, 0.1]))  # (bearing, range)
 
 def add_pose(graph, initial_estimate):
-    # TODO: Add the odometry factor between X(4) and X(5) to the graph (BetweenFactorPose2)
+    # TODO: Add the odometry factor between X(3) and X(4) to the graph (BetweenFactorPose2)
+    # Robot rotates 45°, moves 2m, rotates 45° more
+    dx     = 2.0 * math.cos(math.pi / 4)  # ≈ 1.414
+    dy     = 2.0 * math.sin(math.pi / 4)  # ≈ 1.414
+    dtheta = math.pi / 2                   # 90° total rotation
+    graph.add(gtsam.BetweenFactorPose2(X(3), X(4), gtsam.Pose2(dx, dy, dtheta), ODOMETRY_NOISE))
 
-    # TODO: Based on the odometry, find the initial estimate for the pose of X(5) and add it to the graph
-    
+    # TODO: Based on the odometry, find the initial estimate for the pose of X(4) and add it to the graph
+    # Take X(3)'s current estimate and apply the relative motion
+    pose_3 = gtsam.Pose2(4.0, 0.0, 0.0)
+    pose_4 = pose_3.compose(gtsam.Pose2(dx, dy, dtheta))
+    initial_estimate.insert(X(4), pose_4)
+
     return graph, initial_estimate
